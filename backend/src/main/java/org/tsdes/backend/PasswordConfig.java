7c85ead94897d97fffef49f0a24960e0f935901b
WhoCanThisBe <Wen.hhp@gmail.com>
2021-09-28 11:46:00 +0200

    test

--- backend/src/main/java/org/tsdes/backend/PasswordConfig.java
+++ backend/src/main/java/org/tsdes/backend/PasswordConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class PasswordConfig {
-    /*test*/
+    /*twerwrst*/
 
     @Bean
     public PasswordEncoder passwordEncoder(){
